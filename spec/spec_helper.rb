8a14b458f473219e974d81cfcc5bc31f46b4d70d
Ivan Valdes <ivan@ooyala.com>
2010-12-02 17:56:25 -0600

    Updating rspec version support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,9 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'lunetas'
-require 'spec'
-require 'spec/autorun'
+require 'rspec'
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   
 end
 
